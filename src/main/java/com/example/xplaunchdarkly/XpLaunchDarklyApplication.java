046f50a8019fef0613def2c3b6dd6c0efd2aebb6
naderkahwaji <nader.kahwaji@gmail.com>
2023-03-29 01:20:16 +0300

    init commit

---
+++ src/main/java/com/example/xplaunchdarkly/XpLaunchDarklyApplication.java
@@ -0,0 +1,13 @@
+package com.example.xplaunchdarkly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XpLaunchDarklyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XpLaunchDarklyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/xplaunchdarkly/XpLaunchDarklyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.xplaunchdarkly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XpLaunchDarklyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
